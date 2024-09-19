3e4c659751bc73844a159043b3cb9d9fe4fb57a3
Your Name <you@example.com>
2024-09-19 18:36:43 +0200

    Display menu method

---
+++ App.java
@@ -0,0 +1,32 @@
+public void displayMenu() {
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+        System.out.println("\nWelcome to the Bank Application!");
+        System.out.println("\n1. Check Balance");
+        System.out.println("2. Deposit Money");
+        System.out.println("3. Withdraw Money");
+        System.out.println("4. Exit");
+        System.out.print("\nPlease make a choice (1-4): \n");
+        String choice = scanner.nextLine();
+        switch (choice) {
+            case "1":
+                checkBalance();
+                break;
+            case "2":
+                depositMoney(scanner);
+                break;
+            case "3":
+                withdrawMoney(scanner);
+                break;
+            case "4":
+                System.out.println("Exiting the program.");
+                scanner.close();
+                return;
+            default:
+                System.out.println("INVALID CHOICE!!!, please try again.");
+        }
+    }
+}
+private void checkBalance() {
+    System.out.printf("Your balance is: %.2f kr%n \n", balance);
+}
